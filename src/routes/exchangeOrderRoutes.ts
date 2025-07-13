1f5e33364d4206a1f17808cc8b5c2716580f917e
Hamid <h.sorkhroo@gmail.com>
2025-07-13 09:43:14 +0330

    exchange route bugs fixed

--- src/routes/exchangeOrderRoutes.ts
+++ src/routes/exchangeOrderRoutes.ts
@@ -13,26 +13,28 @@ import { authenticateUser } from "../middleware/authMiddleware";
 const router = Router();
 
 router.post("/", authenticateUser, createExchangeOrder); // ایجاد سفارش جدید
-router.get("/:id", authenticateUser, getExchangeOrder); // دریافت سفارش با آیدی
+router.get("/orders/list", authenticateUser, getNewOrders); //get new orders
+router.get(
+  "/orders/disputed",
+  authenticateUser,
+  getDisputedOrders
+); //get all disputed orders
+router.get(
+  "/orders/:id",
+  authenticateUser,
+  getExchangeOrder
+); // دریافت سفارش با آیدی
 router.get(
   "/user/:id",
   authenticateUser,
   getExchangeOrdersByUserId
 ); // دریافت کلیه سفارشات با آیدی
+
+router.patch("/:id", authenticateUser, updateOrderStatus); //update order status
 router.patch(
   "/:id/pay",
   authenticateUser,
   updatePaymentRef
 ); // ثبت شماره پیگیری پرداخت
-//get all disputed orders
-router.get(
-  "/disputed",
-  authenticateUser,
-  getDisputedOrders
-);
-//get new orders
-router.get("/list", authenticateUser, getNewOrders);
-//update order status
-router.patch("/:id", authenticateUser, updateOrderStatus);
 
 export default router;
